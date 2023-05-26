aab75f5fb0d03f2e8293bbf77155a08ec436ca78
“Kristoffer <kristoffer.waite@bronxsoftware.org>
2023-05-26 12:32:09 -0400

    fixed error in route file

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -1,8 +1,20 @@
-children: [
-  { path: '', component: () => import('src/pages/HomePage.vue'), name: 'Home' },
-  { path: 'poll/:user', component: () => import('src/pages/PollPage.vue'), name: 'Poll' },
-  { path: 'results', component: () => import('src/pages/ResultsPage.vue'), name: 'Results' }
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('src/pages/HomePage.vue'), name: 'Home' },
+      { path: 'poll', component: () => import('src/pages/PollPage.vue'), name: 'Poll' },
+      { path: 'results', component: () => import('src/pages/ResultsPage.vue'), name: 'Results' }
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue')
+  }
 ]
 
-
-
+export default routes
