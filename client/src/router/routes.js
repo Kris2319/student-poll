063a11816ec77490f4a11c0e59ddd282cc274043
“Kristoffer <kristoffer.waite@bronxsoftware.org>
2023-05-26 12:10:30 -0400

    added school reference to student poll application

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -1,20 +1,8 @@
-const routes = [
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('src/pages/HomePage.vue'), name: 'Home' },
-      { path: 'poll/:user', component: () => import('src/pages/PollPage.vue'), name: 'Poll' },
-      { path: 'results', component: () => import('src/pages/ResultsPage.vue'), name: 'Results' }
-    ]
-  },
-
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
+children: [
+  { path: '', component: () => import('src/pages/HomePage.vue'), name: 'Home' },
+  { path: 'poll/:user', component: () => import('src/pages/PollPage.vue'), name: 'Poll' },
+  { path: 'results', component: () => import('src/pages/ResultsPage.vue'), name: 'Results' }
 ]
 
-export default routes
+
+
